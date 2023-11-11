28d2d363f54435d419360124bcbf897c8b7394ef
rikaalii <rikaali0602.gmail.com>
2023-11-11 22:52:11 +0700

    login

---
+++ app/src/Login/Loginmenu.kts
@@ -0,0 +1,16 @@
+package com.aplikasi.laudry;
+
+import ...
+
+public class login extends AppCompatActivity {
+    EditText txUser, txtPass;
+    Button btnlogin;
+    Spinner Spinner;
+
+    @SuppressLint("MissingInflatedId")
+    @override
+    protected void onCreate(Bundle savedInstancestate) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.la)
+    }
+}
