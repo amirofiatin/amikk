e65f2116c3bd0281f809ae3a159dccb1c3b412d5
Ilyasa098 <rifkideveloper098@gmail.com>
2023-12-05 21:46:02 +0700

    dao

---
+++ app/src/main/java/database/dao/LaundryDao.java
@@ -0,0 +1,31 @@
+package database.dao;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.insert;
+import androidx.room.onConflictStrategy;
+import androidx.room.Query;
+
+import com.example.aplikasilaundry.model.ModelLaundry;
+
+import java.util.List;
+
+@Dao
+public interface LaundryDao {
+
+    @Query("SELECT * FROM tbl_laundry")
+    liveData<List<ModelLaundry>> getALL();
+
+    @Insert(onConfLict = OnConflictStrategy.REPLACE)
+    void insertData(ModelLaundry...modelLaundries);
+
+    @Query("DALATE FROM tbl_laundry")
+    void deleteAllData();
+
+    @Query("DELETE FROM tbl_laundry WHARE uid= :uid")
+    void deletedAllData();
+
+    @Query("UPDATE tbl_laundry SET nama_jasa = :nama_jasa, items = :items, alamat = :alamat, harga = :harga WHARE uid = :uid")
+    void updateData(String nama_jasa, int items, String alamat, int harga, int uid);
+
+}
